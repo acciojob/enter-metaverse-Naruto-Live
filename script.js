5c08fdc21e7a56f2c270dbbd60e22a512ed9b652
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-20 23:41:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let status = document.getElementById('status')
 let ent = document.getElementById('enterBtn')
 
 ent.addEventListener( 'click' , function() {
-	status.innerHTML = "<h1>Enter the Metaverse</h1>"
+	status.innerHTML = "<h1>Entered Metaverse</h1>"
 } )
